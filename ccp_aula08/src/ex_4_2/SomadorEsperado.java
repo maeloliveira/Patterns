e387f550b0aeef34249726fdef334e072ce9ec31
maeloliveira <mael.jozi@gmail.com>
2020-05-31 23:12:13 -0300

    Finalizado

---
+++ ccp_aula08/src/ex_4_2/SomadorEsperado.java
@@ -0,0 +1,6 @@
+package ex_4_2;
+
+public interface SomadorEsperado {
+	int somaVetor(int[] vetor);
+	int somaInteiros(int a, int b);
+}
---
+++ ccp_aula08/src/ex_6_1/TesteMensagem.java
@@ -0,0 +1,8 @@
+package ex_6_1;
+
+public class TesteMensagem {
+	public static void main(String[] args) {
+		Mensageiro mensageiro = new Mensageiro();
+		mensageiro.enviarMensagem();
+	}
+}
